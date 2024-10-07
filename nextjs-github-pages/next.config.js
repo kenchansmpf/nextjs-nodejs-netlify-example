7937efa660a3f215cdee04458beab012fe8b2326
Ken <ken.chan.sm.pf@gmail.com>
2024-10-07 15:32:53 +0800

    update for github page exampl

--- nextjs-github-pages/next.config.js
+++ nextjs-github-pages/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true
+  output: "export",
+  basePath: "/gh-pages-test",
 };
 
 module.exports = nextConfig;
